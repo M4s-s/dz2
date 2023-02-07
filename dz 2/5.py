6b76a6633039fd71a4792cca512b1358c4c45cc7
Даниил <dtrofimov20005@gmail.com>
2023-02-08 03:21:25 +0800

    Initial commit

---
+++ dz 2/5.py
@@ -0,0 +1,5 @@
+x=0
+n = int(input())
+for i in range(n):
+    x += int(input())
+print(x)
---
+++ dz 2/9.py
@@ -0,0 +1,9 @@
+n = int(input())
+p=0
+for i in range(n):
+    a = int(input())
+    if a == 0:
+        p+=1
+    else:
+        p+=0
+print(p)
